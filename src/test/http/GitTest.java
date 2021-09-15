6380eaa4449a547d45ce40cf4e5a9dd6204104a6
shk1346 <89139639+shk1346@users.noreply.github.com>
2021-09-15 20:09:31 +0900

    Update GitTest.java

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,8 +2,8 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "local GIT Test용 클래스 입니다. []";
-	private static final String DEV = "dev 브랜치 테스트 시작";
-	private static final String DEV_FIN = "dev 브랜치 테스트 종료";
+	private static final String DESC = "local GIT Test용 클래스 입니다. [test]";
+	private static final String DEV = "dev 브랜치 테스트 시작[test]";
+	private static final String DEV_FIN = "dev 브랜치 테스트 종료[test]";
 
 }
