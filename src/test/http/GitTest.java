8edb9d428d7330419c533bb5850f9b8f229c770e
shk1346 <shkk1346@naver.com>
2021-09-15 20:09:08 +0900

    [김수현] conflict test

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,8 +2,8 @@ package test.http;
 
 public class GitTest {
 	
-	private static final String DESC = "local GIT Test용 클래스 입니다.";
-	private static final String DEV = "dev 브랜치 테스트 시작";
-	private static final String DEV_FIN = "dev 브랜치 테스트 종료";
+	private static final String DESC = "local GIT Test용 클래스 입니다. [local]";
+	private static final String DEV = "dev 브랜치 테스트 시작 [local]";
+	private static final String DEV_FIN = "dev 브랜치 테스트 종료 [local]";
 
 }
